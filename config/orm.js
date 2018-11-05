f562187f0a4ce6474126b200473e3a133030e522
unknown <jayng168@gmail.com>
2018-11-05 09:21:02 -0500

    Finished with user functionality

--- config/orm.js
+++ config/orm.js
@@ -64,11 +64,26 @@ var orm = {
 
             cb(result);
         });
+    },
+
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
-
-    // updateOne: function(table,){
-
-    // }
 };
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,41 +20,28 @@ router.post("/api/burgers", function (req, res) {
     burger.insert([
         "burger_name", "devoured"
     ], [
-            req.body.burger, 1
+            req.body.burger, 0
         ], function (result) {
             // Send back the ID of the new quote
             res.json({ id: result.insertId });
         });
 });
 
-// router.put("/api/burger/:id", function (req, res) {
-//     var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
-//     console.log("condition", condition);
+    console.log("condition", condition);
 
-//     burger.update({
-//         sleepy: req.body.sleepy
-//     }, condition, function (result) {
-//         if (result.changedRows == 0) {
-//             // If no rows were changed, then the ID must not exist, so 404
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     });
-// });
-
-// router.delete("/api/burger/:id", function (req, res) {
-//     var condition = "id = " + req.params.id;
-
-//     burger.delete(condition, function (result) {
-//         if (result.affectedRows == 0) {
-//             // If no rows were changed, then the ID must not exist, so 404
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     });
-// });
+    burger.update({
+        devoured: 1
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
